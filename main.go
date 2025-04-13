b8902e6c7582307e27c72ac129fe2ebbe007338d
Khoa Huynh <khoahuynh.toronto@gmail.com>
2025-04-12 20:20:04 -0400

    run as web server

--- main.go
+++ main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"net/http"
 	"os"
+
+	"github.com/a-h/templ"
 )
 
 func main() {
 	component := hello("John")
 	component.Render(context.Background(), os.Stdout)
+
+	http.Handle("/", templ.Handler(component))
+
+	fmt.Println("Listening on :3000")
+	http.ListenAndServe(":3000", nil)
 }
